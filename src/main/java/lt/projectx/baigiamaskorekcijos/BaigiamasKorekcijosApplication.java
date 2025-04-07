95d856100b2bd880d57d40c863a4b1c666541012
GediminasM <gedmocss@gmail.com>
2025-04-07 20:53:23 +0300

    Initial commit

---
+++ src/main/java/lt/projectx/baigiamaskorekcijos/BaigiamasKorekcijosApplication.java
@@ -0,0 +1,13 @@
+package lt.projectx.baigiamaskorekcijos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaigiamasKorekcijosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaigiamasKorekcijosApplication.class, args);
+	}
+
+}
